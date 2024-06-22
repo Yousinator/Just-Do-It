de0d2cc06d25ba89a22e18afb681dcda9ccf69bb
Yousinator <y.omusabeh@gmail.com>
2024-06-22 18:31:33 +0300

    fixed some code issues

--- app/src/main/java/dsa_final/data_structures/Queue.java
+++ app/src/main/java/dsa_final/data_structures/Queue.java
@@ -2,12 +2,10 @@ package dsa_final.data_structures;
 
 public class Queue {
     private Node front;
-    private Node rear;
     private LinkedList queueList;
 
     public Queue() {
         this.front = null;
-        this.rear = null;
         this.queueList = new LinkedList();
     }
 
@@ -16,15 +14,8 @@ public class Queue {
             throw new IllegalArgumentException("Node to be added cannot be null.");
         }
 
-        if (isEmpty()) {
-            front = newNode;
-            rear = newNode;
-        } else {
-            rear.setNext(newNode);
-            rear = newNode;
-        }
-
         queueList.insertLast(newNode);
+        front = queueList.getHead();
     }
 
     public void dequeue() {
@@ -34,10 +25,6 @@ public class Queue {
 
         queueList.removeFirst();
         front = queueList.getHead();
-
-        if (front == null) {
-            rear = null;
-        }
     }
 
     public boolean isEmpty() {
